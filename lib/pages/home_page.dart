2a8148e0eb592e26890394ac8185d2290dc75496
hansanasandun2001 <hansanasandun999@gmail.com>
2025-10-10 11:22:58 +0530

    modified the home_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,23 +1,36 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Center(
-          child: Text(
-            'This is Home Page',
-            style: TextStyle(fontSize: 24, color: Colors.black87),
-          ),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              color: Colors.white, // Set your desired background color
+              child: TabBar(
+                tabs: [
+                  Tab(text: 'Fish Collection', icon: Icon(Icons.pool)),
+                  Tab(text: 'Our Services', icon: Icon(Icons.build)),
+                  Tab(text: 'Fish Supplies', icon: Icon(Icons.shopping_cart)),
+                ],
+                indicatorColor: Colors.deepPurple,
+                labelColor: Colors.deepPurple,
+                unselectedLabelColor: Colors.grey,
+              ),
+            ),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  Center(child: Text('Fish Collection Content')),
+                  Center(child: Text('Our Services Content')),
+                  Center(child: Text('Fish Supplies & Equipment Content')),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
